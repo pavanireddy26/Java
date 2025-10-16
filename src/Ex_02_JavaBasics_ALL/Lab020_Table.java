8d2b7c7e3d414b1061b552d82520a54416ffba5d
pavan <muthyalapavanireddy1997@gmail.com>
2025-10-16 18:41:45 +0530

    16 oct 2025

--- src/Ex_02_JavaBasics_ALL/Lab020_Table.java
+++ src/Ex_02_JavaBasics_ALL/Lab020_Table.java
@@ -3,24 +3,24 @@ package Ex_02_JavaBasics_ALL;
 public class Lab020_Table {
     public static void main(String[] args) {
         int a =9;
-        System.out.printf("%d ",a*1);
+        System.out.printf("9*1=%d",a*1);
         System.out.println();
-        System.out.printf("%d",a*2);
+        System.out.printf("9*2=%d " ,a*2);
         System.out.println();
-        System.out.printf("%d",a*3);
+        System.out.printf("9*3=%d " ,a*3);
         System.out.println();
-        System.out.printf("%d",a*4);
+        System.out.printf("9*4=%d " ,a*4);
         System.out.println();
-        System.out.printf("%d",a*5);
+        System.out.printf("9*5=%d",a*5);
         System.out.println();
-        System.out.printf("%d",a*6);
+        System.out.printf("9*6=%d",a*6);
         System.out.println();
-        System.out.printf("%d",a*7);
+        System.out.printf("9*7=%d",a*7);
         System.out.println();
-        System.out.printf("%d",a*8);
+        System.out.printf("9*8=%d",a*8);
         System.out.println();
-        System.out.printf("%d",a*9);
+        System.out.printf("9*9=%d",a*9);
         System.out.println();
-        System.out.printf("%d",a*10);
+        System.out.printf("9*10=%d",a*10);
     }
 }
